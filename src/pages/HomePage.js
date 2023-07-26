f2d27d2266cb470fa942c40fce02e9f39bce156c
namnh <33839115+namnh43@users.noreply.github.com>
2023-07-26 09:29:03 +0700

    separate header

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,4 @@
-import {Header} from "../components/Header";
+import {Header} from "../components/header/Header";
 import {Outlet} from "react-router";
 import {Footer} from "../components/Footer";
 import SearchBar from "../components/SearchBar";
